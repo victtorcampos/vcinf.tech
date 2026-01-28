4722483d5b81092a82e51f24938df0ab0c39b05e
Victor Campos <victtorcampos@users.noreply.github.com>
2026-01-27 23:00:05 -0400

    Update ApplicationConfig with UserDetailsService and AuthManager beans

--- BACKEND/src/main/java/tech/vcinftech/ecosystem/config/ApplicationConfig.java
+++ BACKEND/src/main/java/tech/vcinftech/ecosystem/config/ApplicationConfig.java
@@ -1,26 +1,46 @@
 package tech.vcinftech.ecosystem.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import tech.vcinftech.ecosystem.repository.UserRepository;
 
 @Configuration
+@RequiredArgsConstructor
 public class ApplicationConfig {
+
+    private final UserRepository repository;
 
     @Bean
     public UserDetailsService userDetailsService() {
-        // Resolve o "deprecated" usando encoder real
-        UserDetails user = User.builder()
-                .username("user")
-                .password(passwordEncoder().encode("password"))
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
+        return username -> repository.findByUsername(username)
+                .map(user -> org.springframework.security.core.userdetails.User.builder()
+                        .username(user.getUsername())
+                        .password(user.getPassword())
+                        .roles("USER") // Simples role por enquanto
+                        .build())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
