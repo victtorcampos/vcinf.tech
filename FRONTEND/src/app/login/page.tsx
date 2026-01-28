69dc9632ba12e1e6249f23eb0cdc24888d8563a6
Victor Campos <victtorcampos@users.noreply.github.com>
2026-01-28 14:08:52 -0400

    Fix: Add Navbar explicitly to Login Page

--- FRONTEND/src/app/login/page.tsx
+++ FRONTEND/src/app/login/page.tsx
@@ -1,17 +1,21 @@
+import { Navbar } from "@/components/Navbar";
 import LoginForm from '@/components/LoginForm';
 
 export default function LoginPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-background">
-      <div className="w-full max-w-md p-8 space-y-8 bg-card rounded-lg shadow-lg">
-        <div className="text-center">
-          <h1 className="text-2xl font-bold text-foreground">Área do Cliente</h1>
-          <p className="text-muted-foreground">
-            Acesse sua conta para gerenciar seus serviços.
-          </p>
+    <>
+      <Navbar />
+      <div className="flex items-center justify-center min-h-screen bg-background">
+        <div className="w-full max-w-md p-8 space-y-8 bg-card rounded-lg shadow-lg">
+          <div className="text-center">
+            <h1 className="text-2xl font-bold text-foreground">Área do Cliente</h1>
+            <p className="text-muted-foreground">
+              Acesse sua conta para gerenciar seus serviços.
+            </p>
+          </div>
+          <LoginForm />
         </div>
-        <LoginForm />
       </div>
-    </div>
+    </>
   );
 }
