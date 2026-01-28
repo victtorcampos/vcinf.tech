aed5488ddc0bab2cca65bcc59a3ce216677fb9a4
Victor Campos <victtorcampos@users.noreply.github.com>
2026-01-28 14:08:39 -0400

    Fix: Add Navbar explicitly to Landing Page

--- FRONTEND/src/app/page.tsx
+++ FRONTEND/src/app/page.tsx
@@ -1,16 +1,19 @@
-import { Hero } from '@/components/Hero';
-import { Services } from '@/components/Services';
-import { InfoBar } from '@/components/InfoBar';
-import { Footer } from '@/components/Footer';
+import { Navbar } from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import Services from "@/components/Services";
+import InfoBar from "@/components/InfoBar";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
-      <Hero />
-      <Services />
-      <InfoBar />
-      <Footer />
+      <Navbar />
+      <main className="flex min-h-screen flex-col items-center justify-between">
+        <Hero />
+        <Services />
+        <InfoBar />
+        <Footer />
+      </main>
     </>
   );
 }
-
